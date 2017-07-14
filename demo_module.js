5fcbcb6cc49cb1bb7dcac7a51b4402210077c129
peachy-manasis <peachy.manasis@crmworks.asia>
2017-07-14 15:19:11 +0800

    - read the query string

--- demo_module.js
+++ demo_module.js
@@ -1,8 +1,7 @@
 var http = require('http');
-var dt = require('./mydatetimemodule');
 
 http.createServer(function(req, res)
 {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('The current date and time is: ' + dt.myDateTime());
+	res.write(req.url);
 }).listen(8080);
